3757395483d93c3bd62ef52e6581e18c558f37fe
Zeynep Serra Güneri <63166238+zeynepguneri@users.noreply.github.com>
2024-12-07 19:16:27 +0300

    Update code.js

--- code.js
+++ code.js
@@ -2,14 +2,42 @@ import {Command} from 'commander';
 import chalk from 'chalk';
 import fs from 'fs';
 import readline from 'readline';
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+
+function likidite_func(){
+    fs.readFile('data.json', 'utf8', (err, data) => {
+        if (!data) return; 
+        const jsonData = JSON.parse(data);
+        const pool = jsonData.pool;
+        const userBalance = jsonData.userBalance;
+        const tokenAInput = Number(prompt('Token A miktarını giriniz: '));
+        const tokenBInput = Number(prompt('Token B miktarını giriniz: '));
+    if (userBalance.tokenA < tokenAInput || userBalance.tokenB < tokenBInput) {
+        console.log(chalk.red('Yeterli bakiyeniz yok.'));
+        return;
+      }
+    else {
+        userBalance.tokenA -= tokenAInput;
+        userBalance.tokenB -= tokenBInput;
+        pool.tokenA += tokenAInput;
+        pool.tokenB += tokenBInput;
+        pool.K = pool.tokenA * pool.tokenB;
+        fs.writeFileSync('data.json', JSON.stringify({ pool, userBalance }, null, 2));
+        console.log("Likidite başarıyla eklendi.")
+        console.log('Havuz durumu:', pool);
+        console.log('Yeni bakiyeniz:', userBalance);
+    }
+    });
+    
+}
 
 const program = new Command();
 program
-    .command('menu')
+    .command('menü')
     .description('Kullanıcıya menüyü göster.')
     .action(()=>{
-        console.log('Yapmak istediğiniz işlemi seçiniz:');
-        console.log('1. Likidite ekle');
+        console.log('\n1. Likidite ekle');
         console.log('2. Swap');
         console.log('3. Havuz durumunu görüntüle');
         console.log('4. Bakiye görüntüle');
@@ -20,11 +48,12 @@ const rl = readline.createInterface({
     output: process.stdout
   });
   
-rl.question('Lütfen bir değer girin: ', (answer) => {
+rl.question('Yapmak istediğiniz işlemi seçiniz: ', (answer) => {
     const giris = answer;
     switch(giris){
         case '1':
             console.log("Likidite ekleniyor.")
+            likidite_func()
             break;
         case '2':
             console.log("Swap yapılıyor.")
@@ -42,11 +71,5 @@ rl.question('Lütfen bir değer girin: ', (answer) => {
     }
     rl.close(); 
   });
-  
-
-fs.readFile('data.json', 'utf8', (err, data) => {
-  if (err) throw err;
-  console.log('Dosya içeriği:', data);
-});
 
 program.parse(process.argv);

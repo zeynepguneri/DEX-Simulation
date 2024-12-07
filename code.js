16ff0ddc0e2ff7f49998169be8be89724dfd7138
Zeynep Serra Güneri <63166238+zeynepguneri@users.noreply.github.com>
2024-12-07 17:13:21 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,5 +1,8 @@
 import {Command} from 'commander';
 import chalk from 'chalk';
+import fs from 'fs';
+import readline from 'readline';
+
 const program = new Command();
 program
     .command('menu')
@@ -12,4 +15,20 @@ program
         console.log('4. Bakiye görüntüle');
         console.log('5. Çıkış');
 })
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+rl.question('Lütfen bir değer girin: ', (answer) => {
+    const giris = answer
+    rl.close(); 
+  });
+  
+
+fs.readFile('data.json', 'utf8', (err, data) => {
+  if (err) throw err;
+  console.log('Dosya içeriği:', data);
+});
+
 program.parse(process.argv);

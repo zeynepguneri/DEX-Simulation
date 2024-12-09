9a3a7cfddc05e7cc9a081240be34bb912f0150d7
Zeynep Serra Güneri <63166238+zeynepguneri@users.noreply.github.com>
2024-12-09 19:35:38 +0300

    Update code.js

--- code.js
+++ code.js
@@ -13,21 +13,26 @@ function likidite_func(){
         const userBalance = jsonData.userBalance;
         const tokenAInput = Number(prompt(chalk.gray('Token A miktarını giriniz: ')));
         const tokenBInput = Number(prompt(chalk.gray('Token B miktarını giriniz: ')));
-    if (userBalance.tokenA < tokenAInput || userBalance.tokenB < tokenBInput) {
-        console.log(chalk.red('Yeterli bakiyeniz yok.'));
-        return;
-      }
-    else {
-        userBalance.tokenA -= tokenAInput;
-        userBalance.tokenB -= tokenBInput;
-        pool.tokenA += tokenAInput;
-        pool.tokenB += tokenBInput;
-        pool.K = pool.tokenA * pool.tokenB;
-        fs.writeFileSync('data.json', JSON.stringify({ pool, userBalance }, null, 2));
-        console.log(chalk.green("Likidite başarıyla eklendi."))
-        console.log('Havuz durumu:', pool);
-        console.log('Yeni bakiyeniz:', userBalance);
-    }
+        if (tokenAInput <= 0 || tokenBInput <= 0) {
+            console.log(chalk.red("Geçersiz miktar girdiniz."));
+            return;
+        }else{
+            if (userBalance.tokenA < tokenAInput || userBalance.tokenB < tokenBInput) {
+                console.log(chalk.red('Yeterli bakiyeniz yok.'));
+                return;
+            }
+            else {
+                userBalance.tokenA -= tokenAInput;
+                userBalance.tokenB -= tokenBInput;
+                pool.tokenA += tokenAInput;
+                pool.tokenB += tokenBInput;
+                pool.K = pool.tokenA * pool.tokenB;
+                fs.writeFileSync('data.json', JSON.stringify({ pool, userBalance }, null, 2));
+                console.log(chalk.green("Likidite başarıyla eklendi."))
+                console.log('Havuz durumu:', pool);
+                console.log('Yeni bakiyeniz:', userBalance);
+            }
+        }
     });
 }
 
@@ -41,35 +46,47 @@ function swap_func(){
         const tokenInput = prompt(chalk.gray('Almak istediğiniz token: '));
         if (tokenInput === 'B' && tokenOutput === 'A'){
             const tokenBInput = parseFloat(prompt(chalk.gray('Almak istediğiniz tokenB miktarı: ')));
-            const tokenAOutput = pool.K / (pool.tokenB - tokenBInput) - pool.tokenA;
-            if (tokenAOutput<0){
-                console.log(chalk.red("Yeterli bakiyeniz yok."))
-            }
-            else{
-                userBalance.tokenB += tokenBInput;
-                userBalance.tokenA -= tokenAOutput;
-                pool.tokenA += tokenAOutput;
-                pool.tokenB -= tokenBInput;
-                fs.writeFileSync('data.json', JSON.stringify({ pool, userBalance }, null, 2));
-                console.log(chalk.green("Swap işlemi başarıyla gerçekleşti."))
-                console.log('Havuz durumu:', pool);
-                console.log('Yeni bakiyeniz:', userBalance);
+            if (tokenBInput <= 0) {
+                console.log(chalk.red("Geçersiz miktar girdiniz."));
+                return;
+            }else{
+                const tokenAOutput = pool.tokenA - (pool.K / (pool.tokenB + tokenBInput));
+                if (tokenAOutput>userBalance.tokenA || tokenBInput > pool.tokenB){
+                    console.log(chalk.red("Yeterli bakiye yok."));
+                    return;
+                }
+                else{
+                    userBalance.tokenB += tokenBInput;
+                    userBalance.tokenA -= tokenAOutput;
+                    pool.tokenA += tokenAOutput;
+                    pool.tokenB -= tokenBInput;
+                    fs.writeFileSync('data.json', JSON.stringify({ pool, userBalance }, null, 2));
+                    console.log(chalk.green("Swap işlemi başarıyla gerçekleşti."))
+                    console.log('Havuz durumu:', pool);
+                    console.log('Yeni bakiyeniz:', userBalance);
+                }
             }
         }else if (tokenInput === 'A' && tokenOutput === 'B'){
             const tokenAInput = parseFloat(prompt(chalk.gray('Almak istediğiniz tokenA miktarı: ')));
-            const tokenBOutput = pool.K / (pool.tokenA - tokenAInput) - pool.tokenB;
-            if (tokenBOutput<0){
-                console.log(chalk.red("Yeterli bakiyeniz yok."))
-            }
-            else{
-                userBalance.tokenA += tokenAInput;
-                userBalance.tokenB -= tokenBOutput;
-                pool.tokenA += tokenBOutput;
-                pool.tokenB -= tokenAInput;
-                fs.writeFileSync('data.json', JSON.stringify({ pool, userBalance }, null, 2));
-                console.log(chalk.green("Swap işlemi başarıyla gerçekleşti."))
-                console.log('Havuz durumu:', pool);
-                console.log('Yeni bakiyeniz:', userBalance);
+            if (tokenAInput <= 0) {
+                console.log(chalk.red("Geçersiz miktar girdiniz."));
+                return;
+            }else{
+                const tokenBOutput = pool.tokenB - (pool.K / (pool.tokenA + tokenAInput));
+                if (tokenBOutput>userBalance.tokenB || tokenAInput > pool.tokenA){
+                    console.log(chalk.red("Yeterli bakiye yok."));
+                    return;
+                }
+                else{
+                    userBalance.tokenA += tokenAInput;
+                    userBalance.tokenB -= tokenBOutput;
+                    pool.tokenA += tokenBOutput;
+                    pool.tokenB -= tokenAInput;
+                    fs.writeFileSync('data.json', JSON.stringify({ pool, userBalance }, null, 2));
+                    console.log(chalk.green("Swap işlemi başarıyla gerçekleşti."))
+                    console.log('Havuz durumu:', pool);
+                    console.log('Yeni bakiyeniz:', userBalance);
+                }
             }
         }else {
             console.log(chalk.red("Girdiğiniz token çifti geçersiz."))
@@ -92,6 +109,25 @@ function showBalance() {
         console.log(chalk.green('Güncel bakiyeniz:'));
         console.log('Token A:', userBalance.tokenA);
         console.log('Token B:', userBalance.tokenB);
+    });
+}
+
+function showPool() {
+    fs.readFile('data.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(chalk.red('Havuz verileri okunurken bir hata oluştu.'));
+            return;
+        }
+        if (!data) {
+            console.log(chalk.yellow('Havuz bulunamadı.'));
+            return;
+        }
+        const jsonData = JSON.parse(data);
+        const pool = jsonData.pool;
+        console.log(chalk.green('Havuz durumu:'));
+        console.log('Token A:', pool.tokenA);
+        console.log('Token B:', pool.tokenB);
+        console.log('K: ', pool.K);
     });
 }
 
@@ -124,6 +160,7 @@ rl.question(chalk.blue('Yapmak istediğiniz işlemi seçiniz: '), (answer) => {
             break;
         case '3':
             console.log(chalk.green("Havuz durumu görüntüleniyor."))
+            showPool();
             break;
         case '4':
             console.log(chalk.green("Bakiye görüntüleniyor."))
@@ -137,5 +174,4 @@ rl.question(chalk.blue('Yapmak istediğiniz işlemi seçiniz: '), (answer) => {
     rl.close(); 
   });
 
-program.parse(process.argv);
 program.parse(process.argv);
